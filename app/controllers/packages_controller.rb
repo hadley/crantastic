f01a0434dcb34702a4e9cfe6d979693658e05fe5
hadley <h.wickham@gmail.com>
2007-12-18 01:05:11 +0000

    Package index

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,7 @@ class PackagesController < ApplicationController
   # GET /package
   # GET /package.xml
   def index
-    @package = Package.find(:all)
+    @packages = Package.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
