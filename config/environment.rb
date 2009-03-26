3bfb0e31d92bec3f8015db6410c83858a1e40564
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-26 20:33:38 +0100

    Updated system files to Rails 2.3.2 (rake rails:update)

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,6 +15,6 @@ Rails::Initializer.run do |config|
     :session_key => '_crantastic_session',
     :secret      => 'c4ac317cbe898d0d3f36b98a7817b1139897b106be50e928e96fe26b7b5699cf52cc9cc242ddbd888792c07d61c386b10653804e44e0d64e1cf99f5d9611cccb'
   }
-  
+
   config.active_record.observers = :user_observer
 end
