467039dfaca1ef2a2bc8d6063e891207348c011f
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-27 20:50:57 +0800

    TaggingsController now use resource_controller
    Signed-off-by: hadley wickham <h.wickham@gmail.com>

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -2,95 +2,26 @@ class TaggingsController < ApplicationController
   before_filter :login_required, :only => [ :new, :create  ]
   before_filter :authorization_required, :only => [ :edit, :update  ]
 
+  resource_controller
+
   def authorized?
     tagging = Tagging.find(params[:id])
     tagging.user == current_user
   end
 
-  # GET /tagging
-  # GET /tagging.xml
-  def index
-    @taggings = Tagging.find(:all)
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @tagging }
-    end
-  end
-
-  # GET /tagging/1
-  # GET /tagging/1.xml
-  def show
-    @tagging = Tagging.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @tagging }
-    end
-  end
-
-  # GET /tagging/new
-  # GET /tagging/new.xml
-  def new
+  new_action.before do
     @tagging = Tagging.new
     @tagging.user = current_user
     @tagging.package = Package.find(params[:package_id])
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @tagging }
-    end
   end
 
-  # GET /tagging/1/edit
-  def edit
-    @tagging = Tagging.find(params[:id])
-  end
+  create do
+    flash 'Tagging was successfully created.'
+    wants.html { redirect_to(@tagging.package) }
+    wants.xml { render :xml => @tagging, :status => :created, :location => @tagging }
 
-  # POST /tagging
-  # POST /tagging.xml
-  def create
-    @tagging = Tagging.new(params[:tagging])
-    @tagging.user = current_user
-
-    respond_to do |format|
-      if @tagging.save
-        flash[:notice] = 'Tagging was successfully created.'
-        format.html { redirect_to(@tagging.package) }
-        format.xml  { render :xml => @tagging, :status => :created, :location => @tagging }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @tagging.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /tagging/1
-  # PUT /tagging/1.xml
-  def update
-    @tagging = Tagging.find(params[:id])
-
-    respond_to do |format|
-      if @tagging.update_attributes(params[:tagging])
-        flash[:notice] = 'Tagging was successfully updated.'
-        format.html { redirect_to(@tagging.package) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @tagging.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /tagging/1
-  # DELETE /tagging/1.xml
-  def destroy
-    @tagging = Tagging.find(params[:id])
-    @tagging.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(tagging_url) }
-      format.xml  { head :ok }
+    failure do
+      flash "Tag name did not pass validation - please try again"
     end
   end
 end
