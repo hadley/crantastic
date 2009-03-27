13d04407ec26a4706a7638da166833c24bee1ece
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-27 12:56:27 +0100

    Annotated all models with schema information

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: author
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Author < ActiveRecord::Base
   has_many :versions, :foreign_key => :maintainer_id, :order => :name
   
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: review
+#
+#  id         :integer         not null, primary key
+#  package_id :integer
+#  user_id    :integer
+#  rating     :integer
+#  review     :text
+#  title      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :package
