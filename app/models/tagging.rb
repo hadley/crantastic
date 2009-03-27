92b6e90bfe280cd58f6fee85eb1573acb70741ed
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-27 13:08:26 +0100

    Basic validation for tag format, prevents blank tags etc.

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -13,9 +13,12 @@
 class Tagging < ActiveRecord::Base
   belongs_to :user
   belongs_to :package
-  
+
+  # TODO: this regexp needs to cover more cases
+  validates_format_of :tag, :with => /^[^ ].*[a-zA-Z0-9]$/
+  validates_length_of :tag, :minimum => 2
+
   def self.tags
     connection.execute "SELECT DISTINCT tag, count(*) as count FROM tagging GROUP BY tag"
   end
-  
 end
