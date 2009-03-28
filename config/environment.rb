275dbef1de77ac3dd08c04f09eac5736e60df912
intinno <intinno@intinno.com>
2009-03-28 23:28:57 +0530

    adding redcloth to gems in vendor and adding headings in few pages.

--- config/environment.rb
+++ config/environment.rb
@@ -30,9 +30,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-#  config.gem "will_paginate"
+  # config.gem "will_paginate"
   config.gem 'mislav-will_paginate', :lib => "will_paginate", :source => 'http://gems.github.com' 
-
+  config.gem 'RedCloth', :lib => 'redcloth' #, :source => "http://code.whytheluckystiff.net"
+  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
