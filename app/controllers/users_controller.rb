a354e24c01ed9c68fbf4d096ca75afb12789ca56
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-28 20:56:16 +0100

    Refactored the Users controller to use resource_controller Also somewhat simplified the logic in the activate method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,18 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new(params[:user])
-  end
-  
-  def index
-    @users = User.find :all
-  end
-  
-  def show
-    @user = User.find(params[:id])
+  resource_controller
 
-    respond_to do |format|
-      format.html # show.html.erb
-      # format.xml  { render :xml => @user }
-    end
-  end
-    
-  
-  def create
-    puts "Creating user"
-    cookies.delete :auth_token
-
-    @user = User.new(params[:user])
-    @user.save!
-    
-    redirect_to thanks_path()
-  rescue ActiveRecord::RecordInvalid
-    render :action => 'new'
-  end
+  create.before { cookies.delete :auth_token }
+  create.wants.html { redirect_to thanks_path }
 
   def activate
-    self.current_user = params[:activation_code].blank? ? :false : User.find_by_activation_code(params[:activation_code])
+    self.current_user = User.find_by_activation_code(params[:activation_code])
+
     if logged_in? && !current_user.active?
       current_user.activate
-      flash[:notice] = "Signup complete!  You're now logged in and can start reviewing and tagging."
-    else 
+      flash[:notice] = "Signup complete! You're now logged in and can start reviewing and tagging."
+    else
       flash[:notice] = "Incorrect activation url"
     end
     redirect_back_or_default('/')
   end
-
 end
