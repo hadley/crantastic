14517176f26006779a306eb207c50af26211e5cd
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-29 16:04:02 +0200

    Added AuthHelper module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,6 +42,29 @@ Spec::Runner.configure do |config|
   # config.mock_with :rr
   #
   # == Notes
-  # 
+  #
   # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+end
+
+# Borrowed from mislav's tip over at StackOverflow:
+# http://stackoverflow.com/questions/64827
+module AuthHelper
+  protected
+
+  def login_as(model, id_or_attributes = {})
+    attributes = id_or_attributes.is_a?(Fixnum) ? {:id => id} : id_or_attributes
+    @current_user = stub_model(model, attributes)
+    target = controller rescue template
+    target.instance_variable_set '@current_user', @current_user
+
+    if block_given?
+      yield
+      target.instance_variable_set '@current_user', nil
+    end
+    return @current_user
+  end
+
+  def login_as_user(id_or_attributes = {}, &block)
+    login_as(User, id_or_attributes, &block)
+  end
 end
