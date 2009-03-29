2f706d2ea569d34695bcb121e94075cee620df2e
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-03-29 22:04:57 +0200

    Use the new Package.recent scope + whitespace cleanup

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,51 +1,46 @@
 class PackagesController < ApplicationController
-  @@package_table = Package.table_name
 
   def index
     page_no = params[:page] || 1
     @search_term = String(params[:search])
-    
+
     respond_to do |format|
-      format.html do 
+      format.html do
         @packages = Package.search(@search_term, params[:page])
       end
-      format.js do 
+      format.js do
         @packages = Package.search(@search_term, params[:page])
         render :partial => "packages/list"
       end
-      
+
       format.atom do
-        @packages = Package.find(:all, :order => "#{@@package_table}.created_at", :include => :versions, :conditions => "#{@@package_table}.created_at IS NOT NULL")        
+        @packages = Package.recent
       end
     end
   end
 
-  # GET /packages
-  # GET /packages.xml
   def all
-    #, :include => :versions
     respond_to do |format|
-      format.html do 
+      format.html do
         @packages = Package.all
       end
-      
+
       format.atom do
-        @packages = Package.find(:all, :order => "#{@@package_table}.created_at", :include => :versions, :conditions => "#{@@package_table}.created_at IS NOT NULL")        
+        @packages = Package.recent
+        render :template => "packages/index"
       end
     end
   end
 
-  # GET /package/1
-  # GET /package/1.xml
   def show
     id = params[:id]
-    if (id.to_i != 0) 
+    if (id.to_i != 0)
       pkg = Package.find(id)
       response.headers["Status"] = "301 Moved Permanently"
       redirect_to url_for(pkg)
       return
     end
-    
+
     @package = Package.find_by_name(id.gsub("-", "."))
     @version = @package.latest
 
