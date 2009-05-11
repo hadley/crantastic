fc1ff54ff8e2fa249ecf1a0686ffb7ac3236dde8
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-05-11 10:09:41 +0200

    Up-to-date schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -78,14 +78,14 @@ ActiveRecord::Schema.define(:version => 20090430133227) do
     t.integer  "package_id"
     t.string   "name"
     t.string   "title"
-    t.text     "description",   :limit => 255
-    t.text     "license",       :limit => 255
+    t.text     "description"
+    t.text     "license"
     t.string   "version"
     t.string   "requires"
-    t.text     "depends",       :limit => 255
-    t.text     "suggests",      :limit => 255
+    t.text     "depends"
+    t.text     "suggests"
     t.string   "maintainer"
-    t.text     "author",        :limit => 255
+    t.text     "author"
     t.string   "url"
     t.date     "date"
     t.text     "readme"
