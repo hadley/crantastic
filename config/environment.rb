11be5dc2f8f5ff4bbc52e90b0028c69394ca8dcc
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-01 23:53:18 +0200

    Switched over to using the resource_controller gem instead of the plugin

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,8 @@ Rails::Initializer.run do |config|
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'mislav-will_paginate', :lib => "will_paginate", :source => 'http://gems.github.com'
   config.gem 'markbates-hoptoad_notifier', :lib => "hoptoad_notifier", :source => "http://gems.github.com"
-  config.gem "thoughtbot-factory_girl",  :lib => "factory_girl", :source => "http://gems.github.com"
+  config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+  config.gem "giraffesoft-resource_controller", :lib => "resource_controller", :source => "http://gems.github.com"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
