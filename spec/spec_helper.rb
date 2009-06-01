80717c284a276e38bbc3fb828cc3690cb48f1f4a
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-01 21:04:58 +0200

    No fixtures for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,14 +4,15 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 require 'spec/autorun'
 require 'spec/rails'
+require 'remarkable_rails'
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
   # lines, delete config/database.yml and disable :active_record
   # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  #config.use_transactional_fixtures = true
+  #config.use_instantiated_fixtures  = false
+  #config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
 
   # == Fixtures
   #
