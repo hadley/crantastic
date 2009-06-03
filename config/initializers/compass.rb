db474b97014552bed1f1e6c9539e730fbc4a9671
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-03 02:15:15 +0200

    Added compass

---
+++ config/initializers/compass.rb
@@ -0,0 +1,10 @@
+# Only require Compass for development/testing environments. We simply use the
+# compiled css files when in production and thus cuts down ~7mb on the Heroku
+# slug size (yes, the compass gem is that large!).
+unless RAILS_ENV == "production"
+  require 'compass'
+  # If you have any compass plugins, require them here.
+  Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
+  Compass.configuration.environment = RAILS_ENV.to_sym
+  Compass.configure_sass_plugin!
+end
