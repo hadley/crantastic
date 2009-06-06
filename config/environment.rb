be02fda7cb6cbcd80b98311fcb3e5ca14e227857
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-06 19:03:46 +0200

    Added treetop-dcf gem as a dependency (required by the cron task)

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,7 @@ Rails::Initializer.run do |config|
   config.gem 'haml-edge', :lib => "haml"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem 'gravtastic', :version => '>= 2.1.0'
+  config.gem 'Chrononaut-treetop-dcf', :lib => 'dcf', :source => 'http://gems.github.com'
   config.gem 'grosser-rpx_now', :lib => 'rpx_now', :source => 'http://gems.github.com'
   config.gem 'mislav-will_paginate', :lib => "will_paginate", :source => 'http://gems.github.com'
   config.gem 'markbates-hoptoad_notifier', :lib => "hoptoad_notifier", :source => "http://gems.github.com"
