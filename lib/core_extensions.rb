a312eae4c3b93b7ca980a0c0689f879e9e590767
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-06 03:25:46 +0200

    Added Hash#downcase_keys core extension

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -2,3 +2,14 @@ class Object
   def not_nil?; !nil?; end
   def not_blank?; !blank?; end
 end
+
+class Hash
+  # NOTE: Currently only works for keys that are strings. Should ideally work
+  # for symbols as well.
+  def downcase_keys
+    inject({}) do |options, (key, value)|
+      options[key.downcase] = value
+      options
+    end
+  end
+end
