97e75868f77c193f6ec5cc32445d49dec1beb514
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-09 17:46:24 +0200

    Moved the UpdatePackages class into lib/cran.rb and added 'update_all_packages' task for updating all packages.

--- lib/cran.rb
+++ lib/cran.rb
@@ -7,7 +7,7 @@ require "dcf"
 module CRAN
 
   # Simple struct for one CRAN package
-  class Package < Struct.new(:name, :version)
+  class CranPackage < Struct.new(:name, :version)
     def to_s
       "#{name} (#{version})"
     end
@@ -29,7 +29,7 @@ module CRAN
     def initialize(location)
       @packages = []
       Dcf.parse(Zlib::GzipReader.new(open(location)).read).each do |pkg|
-        @packages << CRAN::Package.new(pkg["Package"], pkg["Version"])
+        @packages << CranPackage.new(pkg["Package"], pkg["Version"])
       end
     end
 
@@ -39,6 +39,78 @@ module CRAN
 
     def [](i)
       @packages[i]
+    end
+  end
+
+  class UpdatePackages
+    # To limit the number of updates per hour to avoid long-running processes,
+    # set =max= to a positive digit.
+    def start(max=-1)
+      Log.log!("Starting cron task: UpdatePackages")
+      known_versions = Package.all
+      latest_versions = CRAN::Packages.new("http://cran.r-project.org/src/contrib/PACKAGES.gz")
+
+      i = 0
+      latest_versions.each do |new|
+        break if i == max
+        cur = known_versions.find { |pkg| pkg.name == new.name }
+        if cur
+          if !cur.latest || (cur.latest.version != new.version.to_s)
+            Log.log!("Updating package: #{new}")
+            add_version_to_db(new)
+            i += 1
+          end
+        else
+          Log.log!("New package: #{new}")
+          Package.create!(:name => new.name) # Start by creating the package entry
+          add_version_to_db(new)
+          i += 1
+        end
+      end
+      Log.log!("Cron task finished.")
+      return true
+    end
+
+    def add_version_to_db(pkg)
+      gz = Zlib::GzipReader.new(open("http://cran.r-project.org/src/contrib/#{pkg.name}_#{pkg.version}.tar.gz"))
+      Archive::Tar::Minitar.unpack(gz, File.join(RAILS_ROOT, "/tmp"))
+      pkgdir = File.join(RAILS_ROOT, "/tmp/#{pkg.name}/")
+
+      data = Dcf.parse(File.read(pkgdir + "DESCRIPTION")).first
+      data = data.downcase_keys.symbolize_keys
+
+      fields = [:title, :license, :description, :author,
+                :maintainer,:date, :url, :depends, :suggests]
+      data.delete_if { |k,v| !fields.include?(k) } # Remove unwanted fields
+
+      data.merge!(read_from_files(pkgdir, %w(README NEWS)))
+      # No standard for what the changelog file should be called so we try a bunch
+      data[:changelog] = read_from_files(pkgdir, %w(CHANGELOG ChangeLog Changes)).values.first
+
+      data.merge!(pkg.to_hash)
+
+      # We must convert every value to UTF-8
+      data = data.inject({}) { |b, (k,v)| b[k] = v.latin1_to_utf8 if v; b }
+
+      # Find or create maintainer
+      data[:maintainer] = Author.new_from_string(data[:maintainer])
+
+      Package.find_by_name(pkg.name).versions << Version.create!(data)
+      FileUtils.rm_rf(pkgdir)
+    end
+
+    def read_from_files(pkgdir, files)
+      data = {}
+      files.each do |f|
+        if File.exists?(pkgdir + f)
+          data[f.downcase.to_sym] = File.read(pkgdir + f)
+        else
+          if File.exists?(pkgdir + "inst/" + f)
+            data[f.downcase.to_sym] = File.read(pkgdir + "inst/" + f)
+          end
+        end
+      end
+      data
     end
   end
 
---
+++ lib/tasks/update_all_packages.rake
@@ -0,0 +1,4 @@
+desc "Update all package versions"
+task :update_all_packages => :environment do
+  CRAN::UpdatePackages.new.start
+end
