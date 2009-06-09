4d079a65cd60451812f82a286f9963429d2e2a2b
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-09 23:29:58 +0200

    Added tag_cloud helper

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,12 @@
 module TagsHelper
+
+  def tag_cloud(tags, classes)
+    max_count = tags.sort_by(&:count).last.count.to_f
+
+    tags.each do |tag|
+      index = ((tag.count / max_count) * (classes.size - 1)).round
+      yield tag, classes[index]
+    end
+  end
+
 end
