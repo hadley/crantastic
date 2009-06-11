97300526d92bddb5f0a0ea7a71859a571269e357
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-11 18:56:48 +0200

    Made will_paginate unobtrusive. jQuery is now used instead of Prototype for the AJAX calls. [Issue #2]

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,3 +6,28 @@ jQuery(function(){
             });
         jQuery('div.rating-cancel').remove();
     });
+
+$(document).ready(function() {
+      $('div.pagination a').livequery('click', function() {
+          $('#packages_list').load(this.href);
+          return false;
+      });
+
+      $('input#search').delayedObserver(function(value, object) {
+              $("#packages_list").load('/packages',
+                                       { search: escape($("input#search").val())
+              });
+              $('#spinner').hide();
+          }, 0.5);
+    });
+
+$(document).ajaxSend(function(event, request, settings) {
+  if (settings.type == 'GET' || settings.type == 'get' || typeof(AUTH_TOKEN) == "undefined") return;
+  // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
+  settings.data = settings.data || "";
+  settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
+});
+
+jQuery.ajaxSetup({
+  'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+})
