31d2c03f31ceed636a4ccfcb4156c00c0033e16f
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-11 19:43:47 +0200

    Safer placement of $(#spinner).show, fixed an edge case

--- public/javascripts/jquery.delayedObserver.js
+++ public/javascripts/jquery.delayedObserver.js
@@ -20,9 +20,9 @@
                     .data('condition', op.condition || function() { return ($(this).data('oldval') == $(this).val()); })
                     .data('callback', callback)
                     [(op.event||'keyup')](function(){
-                        $('#spinner').show(); // Always show spinner while loading
                         if (el.data('condition').apply(el)) { return }
                         else {
+                            $('#spinner').show(); // Always show spinner while loading
                             if (el.data('timer')) { clearTimeout(el.data('timer')); }
                             el.data('timer', setTimeout(function(){
                                 el.data('callback').apply(el);
