be412cb274a78ede0e86e3c9e4128cd6f9a8f578
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-13 00:23:11 +0200

    Enable Webrat in the RSpec test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,27 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.dirname(__FILE__) + "/../config/environment" unless defined?(RAILS_ROOT)
 require 'spec/autorun'
 require 'spec/rails'
-require 'remarkable_rails'
+
+Webrat.configure do |config|
+  config.mode = :rails
+end
+
+module Spec::Rails::Example
+  class IntegrationExampleGroup < ActionController::IntegrationTest
+
+   def initialize(defined_description, options={}, &implementation)
+     defined_description.instance_eval do
+       def to_s
+         self
+       end
+     end
+
+     super(defined_description)
+   end
+
+    Spec::Example::ExampleGroupFactory.register(:integration, self)
+  end
+end
 
 Spec::Runner.configure do |config|
   # If you're not using ActiveRecord you should remove these
