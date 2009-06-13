0d6176646962af536c436707d10efcee5243a5f4
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-13 20:59:58 +0200

    XHTML specs for Author controller

--- spec/controllers/authors_controller_spec.rb
+++ spec/controllers/authors_controller_spec.rb
@@ -7,4 +7,20 @@ describe AuthorsController do
     response.status.should == "404 Not Found"
   end
 
+  describe "XHTML Markup" do
+
+    integrate_views
+
+    it "should have an XHTML Strict compilant index page" do
+      get :index
+      response.body.strip_entities.should be_xhtml_strict
+    end
+
+    it "should have an XHTML Strict compilant show page" do
+      get :show, :id => 1
+      response.body.strip_entities.should be_xhtml_strict
+    end
+
+  end
+
 end
