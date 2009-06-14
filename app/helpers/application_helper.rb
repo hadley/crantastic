60243d45ecad094d643558ff63e381a278f9b967
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-14 16:41:04 +0200

    Cleaned up application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,38 +1,15 @@
 module ApplicationHelper
-  
-  # def error_messages_for(object)  
-  #   if object  
-  #     count = object.errors.count  
-  #   else  
-  #     count = 0  
-  #   end  
-  #   
-  #   return "" if count.zero?
-  # 
-  #   header_message = "The following errors were found:"  
-  #   error_messages = object.errors.map {|msg| content_tag(:li, msg.join(" "))}  
-  #   # We render the messages inside an unordered list inside a div  
-  #   content_tag(:div,  
-  #     content_tag(:p, header_message) + content_tag(:ul, error_messages),  
-  #     :class => 'box'  
-  #   )  
-  # end
-  
+
   def error_message_for(object, field)
     return if object.errors[field].blank?
     errors = object.errors[field]
     errors = errors.join(" and ") if errors.is_a? Array
-        
+
     content_tag :span, errors, :class => :error
-  end
-  
-  # Returns the current environment of rails 
-  def rails_env
-    ENV['RAILS_ENV']
   end
 
   def clear_div
-    %{<div class="clear"></div>}
+    content_tag(:div, nil, :class => :clear)
   end
- 
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe ApplicationHelper do
 
+  it "should have a clear div helper" do
+    helper.clear_div.should == %{<div class="clear"></div>}
+  end
+
 end
