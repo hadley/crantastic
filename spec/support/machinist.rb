5aea594c4b1209a8837b0ec077ed05667951233e
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-24 00:34:30 +0200

    Added 'make_unvalidated' method, for usage with Machinist

--- spec/support/machinist.rb
+++ spec/support/machinist.rb
@@ -1,3 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + "/../blueprints")
 
 Spec::Runner.configure { |config| config.before(:each) { Sham.reset } }
+
+class ActiveRecord::Base
+  def self.make_unvalidated(*args)
+    self.make_unsaved(*args).save(false)
+  end
+end
