5e2617c82c5613e9fd8af30c6144f9789788d171
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-24 03:14:44 +0200

    Update the author's updated_at attribute when new versions are released

--- app/models/version_observer.rb
+++ app/models/version_observer.rb
@@ -1,9 +1,14 @@
 class VersionObserver < ActiveRecord::Observer
 
-  # Cache latest version in the package record
   def after_create(version)
+    # Cache latest version in the package record
     version.package.latest_version = version
     version.package.save # no need to explicitly update updated_at
+
+    # Update the author's updated_at attribute
+    if version.maintainer
+      version.maintainer.update_attribute(:updated_at, Time.now)
+    end
   end
 
 end
