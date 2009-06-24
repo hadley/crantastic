e7e762dd80a266120362097585173995e759ff88
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-24 19:33:26 +0200

    Added "imports" to Version, + specs and a little refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090623170341) do
+ActiveRecord::Schema.define(:version => 20090624172625) do
 
   create_table "author", :force => true do |t|
     t.string   "name"
@@ -170,6 +170,7 @@ ActiveRecord::Schema.define(:version => 20090623170341) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "maintainer_id"
+    t.string   "imports"
   end
 
 end
