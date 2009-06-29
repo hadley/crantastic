e731f05c109fb3f6e9ab0edcadb70ff4fec062a6
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-29 19:01:13 +0200

    Display reverse relationships on the package pages (Issue #15)

--- app/helpers/versions_helper.rb
+++ app/helpers/versions_helper.rb
@@ -1,5 +1,11 @@
 module VersionsHelper
 
+  def package_links(packages)
+    packages.collect do |p|
+      link_to(p.name, p)
+    end.join(", ")
+  end
+
   def version_uses
     ((@version.depends + @version.imports).sort.map do |p|
        link_to p.name, p
