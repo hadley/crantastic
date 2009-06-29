44860a6eb54442967d8164c04cd5081fde60cdef
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-29 17:54:36 +0200

    Added migration for habtm relationships between versions and packages (depends, suggests, enhances)

---
+++ db/migrate/20090629122633_package_depends_enhances_suggests.rb
@@ -0,0 +1,40 @@
+class PackageDependsEnhancesSuggests < ActiveRecord::Migration
+  def self.up
+    create_table :required_package_version, :id => false do |t|
+      t.integer :version_id, :null => false
+      t.integer :required_package_id, :null => false
+    end
+
+    add_index :required_package_version, [:version_id, :required_package_id],
+                                          :unique => true
+
+    create_table :enhanced_package_version, :id => false do |t|
+      t.integer :version_id, :null => false
+      t.integer :enhanced_package_id, :null => false
+    end
+
+    add_index :enhanced_package_version, [:version_id, :enhanced_package_id],
+                                          :unique => true
+
+    create_table :suggested_package_version, :id => false do |t|
+      t.integer :version_id, :null => false
+      t.integer :suggested_package_id, :null => false
+    end
+
+    add_index :suggested_package_version, [:version_id, :suggested_package_id],
+                                          :unique => true
+
+    # Create associations for existing data
+    Version.all.each do |v|
+      v.depends.each  { |i| v.required_packages  << i }
+      v.enhances.each { |i| v.enhanced_packages  << i }
+      v.suggests.each { |i| v.suggested_packages << i }
+    end
+  end
+
+  def self.down
+    drop_table :required_package_version
+    drop_table :enhanced_package_version
+    drop_table :suggested_package_version
+  end
+end
