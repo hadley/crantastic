86747044f3875c1452d48dfc625f03bfa9e0a752
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-06-29 12:47:52 +0200

    Go straight to the package if the search was an exact match of a package name (Issue #8)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,19 @@
 class SearchController < ApplicationController
   def show
     @title = 'Search'
-    @search_term = String(params[:q]).mb_chars.strip.downcase
-    @search_result = Package.search(@search_term)
-    @packages = @search_result.first
 
-    if !@search_term.end_with?('~') && @search_result.size > 1
-      flash[:notice] = "No packages were found for '#{@search_term}'. We tried a fuzzy search instead:"
+    if Package.find_by_name(params[:q])
+      redirect_to :controller => "packages", :action => "show", :id => params[:q]
+    else
+      @search_term = String(params[:q]).mb_chars.strip.downcase
+      @search_result = Package.search(@search_term)
+      @packages = @search_result.first
+
+      if !@search_term.end_with?('~') && @search_result.size > 1
+        flash[:notice] = "No packages were found for '#{@search_term}'. We tried a fuzzy search instead:"
+      end
+
+      @search_term.sub!(/~/, '')
     end
-
-    @search_term.sub!(/~/, '')
   end
 end
