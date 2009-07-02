2a48bc7024121ba01365fbb66de9c7b8675bbc90
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-02 15:17:15 +0200

    Make sure that the tag_cloud helper can't cause a division by zero exception

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,7 +1,8 @@
 module TagsHelper
 
-  def tag_cloud(tags, classes)    
-    max_count = tags.sort_by(&:weight).last.weight.to_f
+  def tag_cloud(tags, classes)
+    # Minimum 1, to prevent division by zero
+    max_count = [tags.sort_by(&:weight).last.weight, 1].max.to_f
 
     tags.each do |tag|
       index = ((tag.weight / max_count) * (classes.size - 1)).round
