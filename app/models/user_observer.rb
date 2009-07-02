cc1a3978850b4c7efcce2e9c5ace34f5d13fd680
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-02 12:11:19 +0200

    Cache compiled markdown for user profiles

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -14,4 +14,9 @@ class UserObserver < ActiveRecord::Observer
   def after_save(user)
     UserMailer.deliver_activation(user) if user.pending?
   end
+
+  def before_save(user)
+    # Cache compiled Markdown
+    user.profile_html = Maruku.new(user.profile).to_html
+  end
 end
