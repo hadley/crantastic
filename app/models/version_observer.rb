0b4b909d990b8743316180e2c2a99c7d5073124b
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-03 16:29:10 +0200

    Reduced the number of UPDATE statements executed by the VersionObserver by one

--- app/models/version_observer.rb
+++ app/models/version_observer.rb
@@ -3,7 +3,13 @@ class VersionObserver < ActiveRecord::Observer
   def after_create(version)
     # Cache latest version in the package record
     version.package.latest_version = version
-    version.package.save # no need to explicitly update updated_at
+
+    # Store associations for depends, enhances, and suggests
+    version.required_packages = version.parse_depends
+    version.enhanced_packages = version.parse_enhances
+    version.suggested_packages = version.parse_suggests
+
+    version.package.save! # no need to explicitly update updated_at
 
     # Update the author's updated_at attribute
     if version.maintainer
@@ -33,12 +39,6 @@ class VersionObserver < ActiveRecord::Observer
                       :user_id => 146) # 146 = crantastic system user
       end
     end
-
-    # Store associations for depends, enhances, and suggests
-    version.required_packages = version.parse_depends
-    version.enhanced_packages = version.parse_enhances
-    version.suggested_packages = version.parse_suggests
-    version.save
   end
 
 end
