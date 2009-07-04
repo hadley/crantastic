bdae048d5a196930aeda7c7858468041827c2153
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-04 09:09:31 +0200

    Added spec for SessionsController

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,7 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-include AuthHelper
-
 describe SessionsController do
+
+  it "should return status forbidden if accessing rpx_token without a token" do
+    get "rpx_token"
+    response.status.should == "403 Forbidden"
+  end
+
+  describe "routes" do
+
+    it "should have route for rpx now tokens" do
+      route_for(:controller => "sessions", :action => "rpx_token").should == "/session/rpx_token"
+    end
+
+    it "should route to login and logout" do
+      params_from(:get, "/login").should == { :controller => "sessions", :action => "new" }
+      params_from(:get, "/logout").should == { :controller => "sessions", :action => "destroy" }
+    end
+
+  end
 
 end
