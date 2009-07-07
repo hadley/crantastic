58ade576aef7ad7ae5afb58ac2982237674252d7
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-07 18:25:11 +0200

    Added date_formats.rb to initializers

---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,8 @@
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+        :recent => "%I:%M%p",
+        :pretty => "%b %d, %Y",
+        :pretty_long => "%b %d, %Y %I:%M%p",
+        :date_time12 => "%m/%d/%Y %I:%M%p",
+        :date_time24 => "%m/%d/%Y %H:%M",
+        :simple => "%Y/%m/%d"
+      )
