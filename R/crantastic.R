efeb99dda25e1cdc5c64814bcff933de474c74b7
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-13 13:47:56 +0200

    Imported the crantastic R-package

---
+++ R/crantastic.R
@@ -0,0 +1,61 @@
+## Tag a package on crantastic. Tags should be a comma-separated string
+## consisting of tag names (no spaces are allowed).
+`crantastic.tag` <- function(package, tags)
+{
+  postData("crantastic.org",
+           paste("/packages/", package, "/taggings", sep=""),
+           paste(getToken(), "&tag_name=", tags, sep=""))
+}
+
+## Submits all installed packages to http://crantastic.org/.
+`crantastic.sendInstalledPackages` <- function(){
+  ## Excludes packages from base
+  packages <- paste(installed.packages(priority=c("NA", "recommended"))[, c(1)], collapse=",")
+
+  PopCon::postData("crantastic.org", "/votes",
+                   paste(getToken(), "&packages=", packages, sep=""))
+}
+
+###############
+## Utilities ##
+###############
+
+## Sends data to a remote server via HTTP POST.
+`postData` <- function(host, path="/", data, port=80)
+{
+  header <- paste(c(paste("POST", path, "HTTP/1.1"),
+                    paste("Host:", host),
+                    "Content-type: application/x-www-form-urlencoded",
+                    "Accept: application/xml",
+                    paste("Content-length:", length(strsplit(data, "")[[1]])),
+                    "Connection: Keep-Alive"),
+                  collapse="\r\n")
+
+  datatosend <- paste(header, "\r\n\r\n", data, sep="")
+
+  fp <- make.socket(host=host, port=port, server=FALSE)
+  write.socket(fp, datatosend)
+
+  output <- character(0)
+  repeat {
+    ss <- read.socket(fp,loop=FALSE)
+    output <- paste(output,ss,sep="")
+    if (regexpr("\r\n0\r\n\r\n",ss)>-1) break();
+    if (ss == "") break();
+  }
+  close.socket(fp)
+  print(output)
+
+  ## Returns TRUE or FALSE, depending on the returned HTTP Status Code.
+  return(grepl("200 OK|201 Created", output))
+}
+
+`getToken` <- function()
+{
+  if (!exists("crantasticToken")) {
+    print("Please define your crantastic token (e.g. in your Rprofile file).")
+    return(FALSE)
+  } else {
+    return(paste("token=", crantasticToken, sep=""))
+  }
+}
