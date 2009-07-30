666bb99bce4a116235aba32cd70f87939c8e3303
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-30 22:12:25 +0200

    Make sure we are at the login url when logging in with webrat

--- spec/support/webrat.rb
+++ spec/support/webrat.rb
@@ -4,6 +4,7 @@ end
 
 module WebratHelpers
   def login_with_valid_credentials(login = "john", password = "test")
+    visit login_url
     fill_in "login", :with => login
     fill_in "password", :with => password
     click_button "login"
