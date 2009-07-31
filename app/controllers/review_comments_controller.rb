e4f8c60e8f8aed3510ecb681c67c1a9686724d4e
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-07-31 14:26:11 +0200

    Letting everybody comment on reviews. Could need some prettier styles.

--- app/controllers/review_comments_controller.rb
+++ app/controllers/review_comments_controller.rb
@@ -5,7 +5,6 @@ class ReviewCommentsController < ApplicationController
   belongs_to :review
 
   before_filter :login_required, :except => :show
-  before_filter :check_package_authorship, :only => :create
 
   show.failure.wants.html { rescue_404 }
 
@@ -14,12 +13,5 @@ class ReviewCommentsController < ApplicationController
   create.wants.html { redirect_to package_review_url(@review.package, @review) }
 
   create.flash "Your review comment has been saved. Thank you!"
-
-  private
-  def check_package_authorship
-    unless current_user.author_of?(Review.find(params[:review_id]).package)
-      render :text => "Unauthorized", :status => :forbidden
-    end
-  end
 
 end
