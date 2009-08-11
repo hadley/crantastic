8e7b77aa50e590a8a13baaff4ea1147740cf904a
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-11 22:49:17 +0200

    Generalized search result pages + converted from erb to haml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,10 @@ class SearchController < ApplicationController
     @title = 'Search'
 
     @search_term = String(params[:q]).mb_chars.strip
-    @packages = Package.search(@search_term)
-    if Package.find_by_name(params[:q]) && @packages.size == 1
+    @results = Package.search(@search_term)
+
+    # Redirect to package if the result set was unambiguous
+    if Package.find_by_name(params[:q]) && @results.size == 1
       redirect_to :controller => "packages", :action => "show", :id => params[:q]
     end
   end
