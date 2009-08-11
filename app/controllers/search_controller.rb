d771614879c2a9de15bb7b500f047cb4b11b30ec
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-11 17:32:08 +0200

    count -> size

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
 
     @search_term = String(params[:q]).mb_chars.strip
     @packages = Package.search(@search_term)
-    if Package.find_by_name(params[:q]) && @packages.count == 1
+    if Package.find_by_name(params[:q]) && @packages.size == 1
       redirect_to :controller => "packages", :action => "show", :id => params[:q]
     end
   end
