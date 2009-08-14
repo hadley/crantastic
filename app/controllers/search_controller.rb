a66c754d864e5124498131ae073db9d5762cf36d
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-14 13:34:16 +0200

    Filter out '()' from search queries

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
     @title = 'Search'
 
     @search_term = String(params[:q]).mb_chars.strip
+    @search_term.gsub!('()', '') # Not allowed in Solr search queries
     @results = Package.find_by_solr(@search_term, :limit => 50).try(:results) || []
 
     # Redirect to package if the result set was unambiguous
