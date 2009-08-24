698d71837c978deb91bf21ab682e7a04f0bf23c9
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-24 16:06:55 +0200

    Subject for the weekly digests

--- app/models/digest_mailer.rb
+++ app/models/digest_mailer.rb
@@ -4,6 +4,7 @@ class DigestMailer < ActionMailer::Base
     @recipients   = "r-help@r-project.org"
     @from         = "Hadley Wickham <cranatic@gmail.com>"
     @sent_on      = Time.now
+    @subject      = "CRAN (and crantastic) updates this week"
     body[:digest] = digest
   end
 
