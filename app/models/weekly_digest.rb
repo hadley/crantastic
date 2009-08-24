bb8bd39010feb721ebc310134870601ef84222a6
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-24 15:56:56 +0200

    Don't autogenerate the param for weekly digests if it has been explicitly passed in

--- app/models/weekly_digest.rb
+++ app/models/weekly_digest.rb
@@ -24,7 +24,7 @@ class WeeklyDigest < ActiveRecord::Base
 
   validates_format_of :param, :with => /^2\d{3}-[0-5]\d$/
 
-  before_validation lambda { |r| r.param = [Time.now.year, Date.today.cweek].join("-") }
+  before_validation lambda { |r| r.param = [Time.now.year, Date.today.cweek].join("-") if r.param.blank? }
 
   default_scope :order => "id DESC" # Newest digests first
 
