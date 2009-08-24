e073419b48ffccf45d8d7adab20d821fa13fb69f
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-24 16:16:36 +0200

    ActionMailer fix for rails 2.3.3

--- app/models/digest_mailer.rb
+++ app/models/digest_mailer.rb
@@ -2,10 +2,10 @@ class DigestMailer < ActionMailer::Base
 
   def weekly_digest(digest)
     @recipients   = "r-help@r-project.org"
-    @from         = "Hadley Wickham <cranatic@gmail.com>"
+    @from         = "cranatic@gmail.com"
     @sent_on      = Time.now
     @subject      = "CRAN (and crantastic) updates this week"
-    body[:digest] = digest
+    @body[:digest] = digest
   end
 
 end
