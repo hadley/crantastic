7e420f74d0c985c67c0218b42aee3eddfcab815c
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-26 12:32:00 +0200

    Fixes filter_paramter_logging, which is broken in Rails 2.3.3.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,43 @@
 
 class ApplicationController < ActionController::Base
 
+  # Fixes filter_paramter_logging, which is broken in Rails 2.3.3.
+  # NOTE: Remove when Rails 2.3.4 is released!
+  def self.filter_parameter_logging(*filter_words, &block)
+    parameter_filter = Regexp.new(filter_words.collect{ |s| s.to_s }.join('|'), true) if filter_words.length > 0
+
+    define_method(:filter_parameters) do |unfiltered_parameters|
+      filtered_parameters = {}
+
+      unfiltered_parameters.each do |key, value|
+        if key =~ parameter_filter
+          filtered_parameters[key] = '[FILTERED]'
+        elsif value.is_a?(Hash)
+          filtered_parameters[key] = filter_parameters(value)
+        elsif value.is_a?(Array)
+          filtered_parameters[key] = value.collect do |item|
+            case item
+            when Hash, Array
+              filter_parameters(item)
+            else
+              item
+            end
+          end
+        elsif block_given?
+          key = key.dup
+          value = value.dup if value
+          yield key, value
+          filtered_parameters[key] = value
+        else
+          filtered_parameters[key] = value
+        end
+      end
+
+      filtered_parameters
+    end
+    protected :filter_parameters
+  end
+
   # replaces the value to all keys matching /password/i with "[FILTERED]"
   filter_parameter_logging :password
 
