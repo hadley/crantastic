7995679ef641b9a91ccf920e1b6dd0edbb7a91a3
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-08-28 11:36:55 +0200

    Updated from-address for the weekly digests

--- app/models/digest_mailer.rb
+++ app/models/digest_mailer.rb
@@ -2,7 +2,7 @@ class DigestMailer < ActionMailer::Base
 
   def weekly_digest(digest)
     @recipients   = "r-help@r-project.org"
-    @from         = "Hadley Wickham <cranatic@gmail.com>"
+    @from         = "Crantastic <cranatic@gmail.com>"
     @sent_on      = Time.now
     @subject      = "CRAN (and crantastic) updates this week"
     @body[:digest] = digest
