208f417602d6adb7c45dc1cac711c677c8d173d6
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-09-07 09:24:21 +0200

    Fixed spec

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Author do
 
-  should_allow_values_for :name, "John Doe", :allow_nil => true
+  should_allow_values_for :name, "John Doe", :allow_nil => false
   should_validate_length_of :name, :minimum => 2, :maximum => 255
   should_allow_values_for :email, "john@doe.co.uk", "", "X", :allow_nil => true
   should_validate_length_of :email, :minimum => 0, :maximum => 255
