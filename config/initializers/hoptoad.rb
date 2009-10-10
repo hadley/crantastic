03c4cb5d30ec249b970327690175863a2a3db0e3
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-10-10 13:37:42 +0200

    Make sure to filter sensitive environment variables for Hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,11 @@
 HoptoadNotifier.configure do |config|
   config.api_key = ENV['HOPTOAD_API_KEY']
+
+  config.environment_filters << "GMAIL"
+  config.environment_filters << "RECAPTCHA_PRIVATE_KEY"
+  config.environment_filters << "RECAPTCHA_PUBLIC_KEY"
+  config.environment_filters << "TWITTER_SECRET"
+  config.environment_filters << "TWITTER_TOKEN"
+  config.environment_filters << "TWITTER_ASECRET"
+  config.environment_filters << "TWITTER_ATOKEN"
 end
