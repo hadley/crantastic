34a966f34c50855e9f0960e58b3ef989d0152c25
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-04-24 15:07:44 +0200

    TaggingsController no longer use resource_controller, fixed a couple of bugs.

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,53 +1,64 @@
 class TaggingsController < ApplicationController
 
-  resource_controller
-
   protect_from_forgery :except => :create
-  before_filter :login_required
+  before_filter :login_required, :set_object
   before_filter :check_permissions, :only => [ :destroy ]
 
-  belongs_to :package
-
-  create.before do
+  def create
     # params[:tag_name] might be a list of tags, an array of tag instances is
     # returned by the following statement.
     tags = Tag.parse_and_find_or_create(params[:tag_name])
+    @tagging.tag = tags.first # Good enough for now
 
-    # Hack hack hack. Not sure of how I could do this more elegantly with resource_controller
-    if tags.size > 1
-      tags[1..-1].each do |t|
-        Tagging.create!(:user => self.current_user, :tag => t, :package => parent_object)
+    throw ActiveRecord::RecordInvalid if tags.empty?
+
+    tags.each do |t|
+      Tagging.create(:user => self.current_user, :tag => t,
+                     :package => @package)
+    end
+
+    flash[:notice] = 'Package tagged succesfully'
+
+    respond_to do |format|
+      format.html { redirect_to(@package) }
+      format.xml do
+        render :xml => @tagging, :status => :created, :location => @package
       end
     end
 
-    @tagging.tag = tags.first
-    @tagging.user = current_user # Set tagging ownership
-  end
+  rescue
+    flash[:notice] = "Tag name did not pass validation - please try again.
+                     Only alphanumeric characters are allowed. You can only
+                     apply a tag once to a given package."
 
-  create do
-    flash 'Package tagged succesfully'
-
-    wants.html { redirect_to(@package) }
-
-    wants.xml do
-      render :xml => @tagging, :status => :created, :location => @package
-    end
-
-    failure do
-      flash "Tag name did not pass validation - please try again.
-             Only alphanumeric characters are allowed. You can only
-             apply a tag once to a given package."
-
-      wants.xml { render :nothing => true, :status => 400 }
+    respond_to do |format|
+      format.html { render :action  => :new }
+      format.xml  { render :nothing => true, :status => 400 }
     end
   end
 
-  destroy.wants.html { redirect_to user_url(@tagging.user) }
+  def destroy
+    object.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(@tagging.package) }
+      format.xml  { head :ok }
+    end
+  end
+
+  def object
+    @tagging
+  end
 
   private
 
-  def parent_object
-    Package.find_by_param(params[:package_id])
+  def set_object
+    @package = Package.find_by_param(params[:package_id])
+    @tagging = if params[:id]
+                 Tagging.find(params[:id])
+               else
+                 Tagging.new(:user => current_user, :package => @package)
+               end
   end
 
   def single_access_allowed?
