173f2d1ab0c843bb19c3fc22d45628262ecd5812
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-05-27 00:05:56 +0200

    Fixed deprecation warning, session_key -> key

--- config/environment.rb
+++ config/environment.rb
@@ -14,7 +14,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.action_controller.session = {
-    :session_key => '_crantastic_session',
+    :key         => '_crantastic_session',
     :secret      => 'c4ac317cbe898d0d3f36b98a7817b1139897b106be50e928e96fe26b7b5699cf52cc9cc242ddbd888792c07d61c386b10653804e44e0d64e1cf99f5d9611cccb'
   }
 
