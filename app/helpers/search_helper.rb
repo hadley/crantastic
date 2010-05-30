0383954f8d2d1843c9994fd9d29aad51b880e2b0
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-05-30 20:09:32 +0200

    html safe star ratings

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,6 +2,7 @@ module SearchHelper
   def star_rating_for(package)
     # The nbsp's are ugly, but it was the first technique that I got working in
     # both FF, Safari, and Opera.
-    content_tag("span", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", :class => "star-rating-on") * package.average_rating
+    content_tag("span", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".html_safe,
+                :class => "star-rating-on") * package.average_rating
   end
 end
