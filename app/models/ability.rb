0e68f36758e73e428dd013cb58b80baac465057d
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-10-06 15:54:58 +0200

    Added CanCan ability settings

---
+++ app/models/ability.rb
@@ -0,0 +1,28 @@
+class Ability
+
+  include CanCan::Ability
+
+  def initialize(user)
+    if user && !user.admin?   # regular users
+
+      can :read, :all
+
+      can :manage, Review, :user_id => user.id
+      can :manage, ReviewComment, :user_id => user.id
+      can :manage, Tagging, :user_id => user.id
+      can :manage, User, :id => user.id
+
+    elsif user && user.admin? # admins
+
+      can :manage, :all
+
+    else                      # people that arent signed in
+
+      can :read, :all
+
+      can :create, User
+
+    end
+  end
+
+end
