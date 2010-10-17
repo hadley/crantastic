3ca1d0624a67682620317059fc26e486a3376fbf
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-10-17 21:12:46 +0200

    Don't require gems from the test environment file anymore

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -20,12 +20,3 @@ config.action_controller.allow_forgery_protection    = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
-
-config.gem "webrat"
-config.gem "markup_validity"
-config.gem "faker"
-
-config.gem "remarkable_rails", :lib => false
-config.gem "rspec",            :lib => false
-config.gem "rspec-rails",      :lib => false
-config.gem "machinist",        :lib => false
