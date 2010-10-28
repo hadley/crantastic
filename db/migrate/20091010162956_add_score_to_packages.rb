9d97cf089e4e0450835bb1d9f44b406e8b31a67f
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2010-10-28 16:00:20 +0200

    Minor simplification of the score migration

--- db/migrate/20091010162956_add_score_to_packages.rb
+++ db/migrate/20091010162956_add_score_to_packages.rb
@@ -2,7 +2,7 @@ class AddScoreToPackages < ActiveRecord::Migration
   def self.up
     add_column :package, :score, :real
     add_index  :package, :score
-    Package.all.each { |pkg| pkg.update_attribute(:score, pkg.calculate_score) }
+    Package.all.each { |pkg| pkg.update_score! }
   end
 
   def self.down
