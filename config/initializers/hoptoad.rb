536034b45518bb0ae29405ccbbdb2f621a96abd8
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2011-04-02 18:42:49 +0200

    Hoptoad now ignores annoying PROPFIND errors

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,8 @@
 HoptoadNotifier.configure do |config|
   config.api_key = ENV['HOPTOAD_API_KEY']
+
+  # Whitelist of errors that Hoptoad ignores
+  config.ignore_by_filter do |exception_data|
+    exception_data.error_message =~ "ActionController::UnknownHttpMethod: PROPFIND"
+  end
 end
